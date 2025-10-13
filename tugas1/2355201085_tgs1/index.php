f6e91555f76f3b7c3d7aadc244d2859fbe3aa10b
ekal <haikalansr@gmail.com>
2025-10-13 21:05:49 +0700

    2355201085_tgs1

--- tugas1/2355201085_tgs1/index.php
+++ tugas1/2355201085_tgs1/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>form register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light d-flex justify-content-center align-items-center vh-100">
     <div class="card shadow-lg p-4" style="width: 28rem; border-radius: 20px;">
