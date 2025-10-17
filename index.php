1909b917072be4541328d39c9fd728cfd5fc6469
Aldhi <aldhi.albadri@gmail.com>
2025-10-17 09:17:56 +0700

    buat index.php list nama

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List Nama</title>
+</head>
+<body>
+    <h1>List Nama kelas 5 Pagi B</h1>
+    <ul>
+        <li>Aldhi Albadri</li>
+    </ul>
+</body>
+</html>
