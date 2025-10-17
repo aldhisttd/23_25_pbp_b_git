186b674ec543a5fc8587f59218758b2c1573992c
=Salsa <salsabiyy6@gmail.com>
2025-10-17 09:35:11 +0700

    selesai list kelas salsa

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>list nama</title>
+</head>
+<body>
+    <h1>list nama kelas 5 pagi b</h1>
+    <ul>
+        <li>salsabila harahap</li>
+    </ul>
+</body>
+</html>
