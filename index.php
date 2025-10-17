807580d8aecb093d2bfe380c76bc0b3d5142382d
falria <falrian2005pratamasiegar@gmail.com>
2025-10-17 09:36:57 +0700

    selesai

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>list nama</title>
+</head>
+<body>
+    <h1>falrian</h1>
+    <h1>list nama kelas 5 pagi b</h1>
+    <ul>
+        <li>aldhi albadri</li>
+    </ul>
+</body>
+</html>
