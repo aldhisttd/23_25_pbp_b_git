e2a371e5eeab09f50126098863b5634da4c592f7
Afrian Saputra <=afrians941@gmail.com>
2025-10-19 10:28:37 +0700

    Menambah db.php

---
+++ tugas2/2355201072_2355201073_tim_08/db.php
@@ -0,0 +1,16 @@
+<?php
+// UBAH sesuai environment kamu:
+$DB_HOST = '127.0.0.1';   // atau 'localhost'
+$DB_PORT = '3306';        // ganti ke 3307 jika MySQL kamu di 3307
+$DB_USER = 'root';
+$DB_PASS = '';            // default XAMPP: kosong
+$DB_NAME = 'data_buku';
+
+$koneksi = mysqli_init();
+mysqli_options($koneksi, MYSQLI_OPT_CONNECT_TIMEOUT, 5);
+
+if (!mysqli_real_connect($koneksi, $DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, (int)$DB_PORT)) {
+  die('Koneksi gagal: ' . mysqli_connect_error());
+}
+
+mysqli_set_charset($koneksi, 'utf8mb4');
