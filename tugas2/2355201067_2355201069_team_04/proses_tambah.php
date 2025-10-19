328c1b6501c9ebfa6faef8f0fd67621a64c1ef4a
Arafat <muhammadarafatperwira@gmail.com>
2025-10-19 16:01:06 +0700

    mengubah code file proses_tambah.php

--- tugas2/2355201067_2355201069_team_04/proses_tambah.php
+++ tugas2/2355201067_2355201069_team_04/proses_tambah.php
@@ -5,20 +5,23 @@ if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 
-$merk = $_POST['merk'];
-$tipe = $_POST['tipe'];
-$tahun = $_POST['tahun'];
-$warna = $_POST['warna'];
-$harga = $_POST['harga'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $merk  = mysqli_real_escape_string($koneksi, $_POST['merk']);
+    $tipe  = mysqli_real_escape_string($koneksi, $_POST['tipe']);
+    $tahun = mysqli_real_escape_string($koneksi, $_POST['tahun']);
+    $warna = mysqli_real_escape_string($koneksi, $_POST['warna']);
+    $harga = mysqli_real_escape_string($koneksi, $_POST['harga']);
 
-$sql = "INSERT INTO mobil (merk, tipe, tahun, warna, harga)
-        VALUES ('$merk', '$tipe', '$tahun', '$warna', '$harga')";
+    $sql = "INSERT INTO mobil (merk, tipe, tahun, warna, harga)
+            VALUES ('$merk', '$tipe', '$tahun', '$warna', '$harga')";
 
-if (mysqli_query($koneksi, $sql)) {
-    echo "Data berhasil disimpan!<br>";
-    echo "<a href='data_mobil.php'>Lihat Data Mobil</a>";
-} else {
-    echo "Error: " . mysqli_error($koneksi);
+    if (mysqli_query($koneksi, $sql)) {
+        // Redirect otomatis ke halaman data mobil
+        header("Location: data_mobil.php?status=sukses");
+        exit;
+    } else {
+        echo "Error: " . mysqli_error($koneksi);
+    }
 }
 
 mysqli_close($koneksi);
