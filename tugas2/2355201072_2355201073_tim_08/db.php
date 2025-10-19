7648cfe3e9321891b13a7cf56c50ac15d012e980
=Suci <=suciramadani2126@gmail.com>
2025-10-19 11:59:05 +0700

    tes

--- tugas2/2355201072_2355201073_tim_08/db.php
+++ tugas2/2355201072_2355201073_tim_08/db.php
@@ -1,7 +1,7 @@
 <?php
 // UBAH sesuai environment kamu:
 $DB_HOST = '127.0.0.1';   // gunakan 127.0.0.1 agar koneksi TCP
-$DB_PORT = '3307';        // penting! karena MySQL kamu jalan di port 3307
+$DB_PORT = '3306';        // penting! karena MySQL kamu jalan di port 3307
 $DB_USER = 'root';
 $DB_PASS = '';            // default XAMPP: kosong
 $DB_NAME = 'data_buku';
@@ -10,7 +10,7 @@ $koneksi = mysqli_init();
 mysqli_options($koneksi, MYSQLI_OPT_CONNECT_TIMEOUT, 5);
 
 if (!mysqli_real_connect($koneksi, $DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, (int)$DB_PORT)) {
-    die('Koneksi gagal: ' . mysqli_connect_error());
+  die('Koneksi gagal: ' . mysqli_connect_error());
 }
 
 mysqli_set_charset($koneksi, 'utf8mb4');
