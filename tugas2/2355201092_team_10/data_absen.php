9d251ed9f4a48302a9b6cf9e72ce9cc7852b7c82
falria <falrian2005pratamasiegar@gmail.com>
2025-10-21 10:23:31 +0700

    mengganti code data absen

--- tugas2/2355201092_team_10/data_absen.php
+++ tugas2/2355201092_team_10/data_absen.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "db_barbershop");
+$koneksi = mysqli_connect("localhost:8111", "root", "", "db_barbershop");
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
