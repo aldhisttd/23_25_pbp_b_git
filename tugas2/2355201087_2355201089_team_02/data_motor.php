fb20b198213151debad72933ecd103df81bbf53a
=rima <=rimaramadany2005@gmail.com>
2025-10-22 20:18:22 +0700

    selesai

--- tugas2/2355201087_2355201089_team_02/data_motor.php
+++ tugas2/2355201087_2355201089_team_02/data_motor.php
@@ -14,7 +14,7 @@ $result = $conn->query($sql);
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
     body {
-      background: linear-gradient(135deg, #f0f7ff, #ffffff);
+      background: linear-gradient(135deg, #7593b4ff, #ffffff);
       min-height: 100vh;
     }
     .card {
